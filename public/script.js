4a01e9032c95d80f52c60615597e10fb47ce1055
M Nweder <m.neweder@gmail.com>
2025-02-10 12:36:12 +0100

    Update new css file, new js file for buttons functions and new send mail button

---
+++ public/script.js
@@ -0,0 +1,3 @@
+document.getElementById("sendMailButton").addEventListener("click", function() {
+    window.location.href = "mailto:m.neweder@gmail.com?subject=Hello&body=I want to contact you!";
+});
