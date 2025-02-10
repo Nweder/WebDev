6fb640cd76120de184a3dd758225d3cda7722214
M Nweder <m.neweder@gmail.com>
2025-02-10 12:44:18 +0100

    Update back button in contact me

--- public/script.js
+++ public/script.js
@@ -1,3 +1,7 @@
 document.getElementById("sendMailButton").addEventListener("click", function() {
     window.location.href = "mailto:m.neweder@gmail.com?subject=Hello&body=I want to contact you!";
 });
+
+document.getElementById("backHomeButton").addEventListener("click", function() {
+    window.location.href = "../index.html"; 
+});
