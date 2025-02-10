695b927ca97086ab47ea0bee801c89de7590d1f7
M Nweder <m.neweder@gmail.com>
2025-02-10 15:59:59 +0100

    Update send me a mail button in about me

--- public/script.js
+++ public/script.js
@@ -9,3 +9,22 @@ document.getElementById("backHomeButton").addEventListener("click", function() {
 document.getElementById("back-button-about-me").addEventListener("click", function() {
     window.location.href = "../index.html"; 
 });
+
+document.getElementById("back-button-for-fun").addEventListener("click", function() {
+    window.location.href = "../index.html"; 
+});
+
+document.getElementById("back-button-my-projects").addEventListener("click", function() {
+    window.location.href = "../index.html"; 
+});
+
+document.getElementById("sendMailButton").addEventListener("click", function() {
+    let UserEmail = document.getElementById("UserEmail").value;
+    if (UserEmail.trim() === "") {
+        alert("Please enter your email before sending.");
+        return;
+    }
+    let subject = "Request to See Your CV";
+    let body = `Hello, I am ${UserEmail}. I would like to see your CV.`;
+    window.location.href = `mailto:your-email@example.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+});
